caab5dbfeb3ead2d114bc49df2adf093aa8aaffe
MrFuentastic <vfuentes8587@gmail.com>
2017-10-03 01:21:29 -0500

    initial commit

---
+++ app/controllers/api/v2/employees_controller.rb
@@ -0,0 +1,21 @@
+class Api::V2::EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def create
+    @employee = Employee.new(
+                            first_name: params[:first_name],
+                            last_name: params[:last_name],
+                            email: params[:email]
+    )
+    @employee.save
+    render :show
+  end
+  
+  def show
+    @employee = Employee.find(params[:id])
+    
+  end
+end
---
+++ app/views/api/v2/employees/_employee.json.jbuilder
@@ -0,0 +1,13 @@
+json.id employee.id
+json.first_name employee.first_name
+json.last_name employee.last_name
+json.birthday employee.birthdate
+json.email employee.email
+json.address employee.addresses.each do |address|
+    json.id address.id
+    json.address_1 address.address_1
+    json.address_2 address.address_2
+    json.city address.city
+    json.state address.state
+    json.zip address.zip
+end 
